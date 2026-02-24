eabddfbb3635e6b16288abf951bbbf5c7bc56ada
Tejas Rane <rtejas621@gmail.com>
2026-02-24 14:12:26 +0530

    final commit

--- src/Config/firebaseAuth.js
+++ src/Config/firebaseAuth.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  "apiKey": import.meta.env.VITE_APIKEY,
-  "authDomain": import.meta.env.VITE_AUTHDOMAIN,
-  "projectId": import.meta.env.VITE_PROJECTID,
-  "storageBucket": import.meta.env.VITE_STORAGEBUCKET,
-  "messagingSenderId": import.meta.env.VITE_MESSEGINGID,
-  "appId": import.meta.env.VITE_APPID
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSEGINGID,
+  appId: import.meta.env.VITE_APPID
 }
 
 // Initialize Firebase
